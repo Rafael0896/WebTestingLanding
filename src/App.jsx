1616c2effbe145509510deb2dea15c235dc9bae2
Rafael0896 <realvarez54@misena.edu.co>
2025-10-14 22:10:40 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Features from './components/Features/Features';
 import TechStack from './components/TechStack';
 import HowItWorks from './components/HowItWorks';
 import FAQ from './components/FAQ';
@@ -13,6 +14,7 @@ const App = () => {
         <div className="app-container">
             <Navbar />
             <Hero />
+            <Features />
             <TechStack />
             <HowItWorks />
             <FAQ />
