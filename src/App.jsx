47b8fc3feeeb42989849b8f0dfc5816ce35cfd65
Rafael0896 <realvarez54@misena.edu.co>
2025-10-14 19:07:48 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
+import Contact from './components/Contact';
 import './index.css';
 
 const App = () => {
@@ -10,7 +11,7 @@ const App = () => {
             <Navbar />
             <Hero />
             <Footer />
-
+            <Contact/>
         </div>
     );
 };
